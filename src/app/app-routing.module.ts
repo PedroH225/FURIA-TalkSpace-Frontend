4ff87a1889cf23e5a8bb56e5def368efec4c23a7
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2025-05-04 18:18:16 -0300

    mudança de nomes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { RegistroComponent } from './registro/registro.component';
 import { ComunidadeComponent } from './comunidade/comunidade.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'chat', component: ChatListComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent,  },
   { path: 'registrar', component: RegistroComponent },
   { path: 'comunidade', component: ComunidadeComponent },
 
